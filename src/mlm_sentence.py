a9ec278658da1d730d72811cac0eee64bbed610a
leahannah <38763691+leahannah@users.noreply.github.com>
2024-03-14 17:32:51 +0100

    updated dom-masking experiment

--- src/mlm_sentence.py
+++ src/mlm_sentence.py
@@ -2,15 +2,15 @@ import torch
 import os
 
 class MLMSentence:
-    def __init__(self, id, sentence, mask_indices, model, tokenizer, top_n=5):
-        self.id = id
-        self.sentence, self.masked_tokens = self.prep_target(sentence, mask_indices)
+    def __init__(self, data, model, tokenizer, top_n=5):
+        self.id = data['id']
+        self.sentence, self.masked_tokens = self.prep_target(data['sentence'], data['mask_idx'])
         self.num_masks = len(self.masked_tokens)
         self.model = model
         self.tokenizer = tokenizer
         self.tokens = self.tokenizer.tokenize(self.sentence)
         self.top_n = top_n
-        self.top_tokens, self.top_probs = self.compute_mlm_tokens_probs()
+        self.top_fillers, self.top_probs = self.compute_mlm_tokens_probs()
     
     def get_sentence(self):
         return self.sentence
@@ -18,8 +18,8 @@ class MLMSentence:
     def get_masked_tokens(self):
         return self.masked_tokens
 
-    def get_top_tokens(self):
-        return(self.top_tokens)
+    def get_top_fillers(self):
+        return(self.top_fillers)
     
     def get_top_probabilities(self):
      return(self.top_probs)
@@ -31,15 +31,27 @@ class MLMSentence:
         if mask > self.num_masks:
             print(f'Mask {mask} is out of range, only {self.num_masks} tokens masked')
             return
-        return self.top_tokens[mask-1][rank], self.top_probs[mask-1][rank]
+        return self.top_fillers[mask-1][rank], self.top_probs[mask-1][rank]
 
     def get_token_prob_rank(self, token, mask=1):
         rank, prob = -1, 0.0
         if mask > self.num_masks:
             print(f'Mask {mask} is out of range, only {self.num_masks} tokens masked')
             return
-        if token in self.top_tokens[mask-1]:
-            rank = self.top_tokens[mask-1].index(token)
+        if token in self.top_fillers[mask-1]:
+            rank = self.top_fillers[mask-1].index(token)
+            prob = self.top_probs[mask-1][rank]
+        return rank, prob
+
+    def get_dom_prob_rank(self, mask=1):
+        token = self.masked_tokens[0]
+        # assert token == 'a' or token == 'al'
+        rank, prob = -1, 0.0
+        if mask > self.num_masks:
+            print(f'Mask {mask} is out of range, only {self.num_masks} tokens masked')
+            return
+        if token in self.top_fillers[mask-1]:
+            rank = self.top_fillers[mask-1].index(token)
             prob = self.top_probs[mask-1][rank]
         return rank, prob
 
@@ -52,34 +64,24 @@ class MLMSentence:
         # predict
         predictions = self.model(tokens_tensor)[0]
         # get mask token predictions
-        top_tokens, top_probs = [], []
+        top_fillers, top_probs = [], []
         for msk in mask_idx:
             probs = torch.nn.functional.softmax(predictions[0, msk], dim=-1) 
             top_p, top_indices = torch.topk(probs, self.top_n, sorted=True) # get highest probabilites
             top_t = self.tokenizer.convert_ids_to_tokens(top_indices) # get tokens with highest probability
             top_probs.append(top_p.tolist())
-            top_tokens.append(top_t)
-        return top_tokens, top_probs
+            top_fillers.append(top_t)
+        return top_fillers, top_probs
 
     def save_to_file(self, outputfile):
-        if not os.path.exists(outputfile):
-            with open(outputfile, mode='w', encoding='utf-8') as f:
-                f.write('id'+'\t'+'sentence')
-                for i in range(self.num_masks):
-                    f.write('\t'+'masked_token'+str(i+1))
-                    for j in range(self.top_n-1):
-                        f.write('\t'+'predicted_token'+str(j+1))
-                        f.write('\t'+'probability'+str(j+1))
-                    f.write('\t'+'predicted_token'+str(j+2)+'\t'+'probability'+str(j+2))
-                f.write('\n')
         with open(outputfile, mode='a', encoding='utf-8') as f:
             f.write(str(self.id)+'\t'+self.sentence)
             for i in range(self.num_masks):
                 f.write('\t'+self.masked_tokens[i])
                 for j in range(self.top_n-1):
-                    f.write('\t'+self.top_tokens[i][j])
+                    f.write('\t'+self.top_fillers[i][j])
                     f.write('\t'+str(round(self.top_probs[i][j], 4)))
-                f.write('\t'+self.top_tokens[i][-1]+'\t'+str(round(self.top_probs[i][-1], 4)))
+                f.write('\t'+self.top_fillers[i][-1]+'\t'+str(round(self.top_probs[i][-1], 4)))
             f.write('\n')
 
     # helper function to add special tokens to sentence in order to use in MLM
---
+++ src/util.py
@@ -0,0 +1,156 @@
+import re
+import pandas as pd
+from transformers import BertForMaskedLM, BertTokenizer
+
+def load_targets(filename, mode):
+    assert mode in ['mask_dom', 'mask_dobj']
+    with open(filename, mode='r', encoding='utf-8') as f:
+        ids, sentences, dobjects, indices, en_sents = [],[], [], [], []
+        next(f)
+        for line in f:
+            cols = line.split('\t')
+            ids.append(cols[0].strip())
+            en_sents.append(cols[2].strip())
+            sent = cols[1].strip()
+            pattern = r'\[(.*?)\]'
+            matches = re.findall(pattern, sent)
+            do = matches[0]
+            sentences.append(sent)
+            dobjects.append(do)
+            if mode == 'mask_dom':
+                idx = find_dom_index(sent.split())
+            elif mode == 'mask_dobj':
+                idx = find_dobj_indices(sent.split())
+            indices.append([idx])
+    return pd.DataFrame(
+            {'id': ids,
+            'sentence': sentences,
+            'en_sentence': en_sents,
+            'mask_idx': indices,
+            'dobj': dobjects
+            })
+
+def find_dom_index(sent_list, char='['):
+    idx = -1
+    for i, w in enumerate(sent_list):
+        if w[0] == char:
+            idx = i-1
+    if idx < 0:
+        print('Index not found in sentence: {}'.format(sent_list))
+    return idx
+
+def find_dobj_indices(sent_list, start='[', end=']'):
+    for i, w in enumerate(sent_list):
+        if w[0] == start:
+            idx1 = i
+        if w[-1] == end:
+            idx2 = i
+    idx = list(range(idx1, idx2+1))
+    print(idx)
+    if idx == []:
+        print('Index not found in sentence: {}'.format(sent_list))
+    return idx
+
+def initialize_result_file(outputfile, num_mask, top_n):
+    with open(outputfile, mode='w', encoding='utf-8') as f:
+        f.write('id'+'\t'+'sentence')
+        for i in range(num_mask):
+            f.write('\t'+'masked_token'+str(i+1))
+            for j in range(top_n-1):
+                f.write('\t'+'predicted_token'+str(j+1))
+                f.write('\t'+'probability'+str(j+1))
+            f.write('\t'+'predicted_token'+str(j+2)+'\t'+'probability'+str(j+2))
+        f.write('\n')
+
+# setup tokenizer and model
+def load_model(modelname):
+    tokenizer = BertTokenizer.from_pretrained(modelname, do_lower_case=False)
+    model = BertForMaskedLM.from_pretrained(modelname)
+    return tokenizer, model
+
+# prepare targets for MLM experiments
+def prep_dom_masking(sentences, indices):
+    tokens_masked = []
+    for i, s  in enumerate(sentences):
+        idx = indices[i]
+        s = s.replace('[', '').replace(']', '')
+        s_list = s.split()
+        toks = []
+        for masked_index in idx:
+            toks.append(s_list[masked_index])
+            s_list[masked_index] = '[MASK]' 
+        s = ' '.join(s_list)
+        s = '[CLS] ' + s + ' [SEP]'
+        sentences[i] = s
+        tokens_masked.append(toks)
+    return sentences, tokens_masked
+
+def get_masked_indices(tokenized):
+    masked_indices = []
+    for i, token in enumerate(tokenized):
+        if token == '[MASK]':
+            masked_indices.append(i)
+    return masked_indices
+
+# get input into correct format
+def prep_input(inputfile, outputfile):
+    with open(inputfile, mode='r', encoding='utf-8') as f:
+        with open(outputfile, mode='w', encoding='utf-8') as out_f:
+            for line in f.readlines():
+                cols = line.split('\t')
+                sentence = cols[1]
+                print(sentence)
+                pattern = r'(\w+\s+\w+)\s+/\s+(\w+\s+\w+)'
+                replacement = r'[\1]'
+                new_sentence = re.sub(pattern, replacement, sentence)
+                print(new_sentence)
+                cols[1] = new_sentence
+                out_f.write('\t'.join(cols))
+
+# get targets into correct format
+def prep_input2(inputfile, outputfile, single_token_dobj=False):
+    with open(outputfile, mode='w', encoding='utf-8') as out_f:
+        out_f.write('id\tes\ten\n')
+        with open(inputfile, mode='r', encoding='utf-8') as f:
+            next(f)
+            cnt = 0
+            for i, line in enumerate(f.readlines()):
+                if i % 2 == 0:
+                    sentence = line.strip()
+                    cnt += 1
+                    sent_list = sentence.split()
+                    dobj1 = '[' + sent_list[5] 
+                    if single_token_dobj:
+                        dobj1 += ']'
+                    sent_list[5] = dobj1
+                    if not single_token_dobj:
+                        dobj2 = sent_list[6] + ']'
+                        sent_list[6] = dobj2
+                    sentence = ' '.join(sent_list)
+                    print(cnt)
+                    print(sentence)
+                else:
+                    en_sentence = line.strip()[1:-1]
+                    print(en_sentence)
+                    out_f.write('\t'.join([str(cnt), sentence, en_sentence]))
+                    out_f.write('\n')
+
+def mark_dobj_en(file, start, end):
+    sentences, en_sentences, ids = [], [], []
+    with open(file, mode='r', encoding='utf-8') as f:
+        next(f)
+        for line in f:
+            cols = line.split('\t')
+            ids.append(cols[0])
+            sentences.append(cols[1])
+            en_sent = cols[2]
+            print(en_sent)
+            sent_list = en_sent.strip('\n').split()
+            sent_list[start] = '[' + sent_list[start]
+            sent_list[end] = sent_list[end] + ']'
+            en_sent = ' '.join(sent_list)
+            en_sentences.append(en_sent)
+            print(en_sent)
+    df = pd.DataFrame({'id': ids, 'es': sentences, 'en': en_sentences})
+    print(df.head())
+    return df
