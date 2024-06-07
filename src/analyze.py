a6336339e5b6267889236c4aad666dd9cc710db9
leahannah <gauner.erle@gmail.com>
2024-06-07 17:06:55 +0200

    add some boxplots

--- src/analyze.py
+++ src/analyze.py
@@ -6,7 +6,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def create_barplot(inpath, title, filename, errorbar=False):
+def create_barplot(inpath, modelname, measure, errorbar=False):
+    # plt.clf()
     df = pd.read_csv(inpath, sep='\t')
     print(df.head())
     print(df.shape)
@@ -15,8 +16,6 @@ def create_barplot(inpath, title, filename, errorbar=False):
         filler_types = df['filler_type'].unique()
     else:
         filler_types = ['sent_score']
-    sns.set_context('paper', rc={'font.size':15,'axes.titlesize':15,'axes.labelsize':12})
-    plt.clf()
     df['input_condition'] = df['input'] + '_' + df['condition']
     mapping = {'dom': 'DOM', 'def': 'definite article', 'indef': 'indefinite article', 'sent_score': 'sentence score'}
     for ftype in filler_types:
@@ -24,15 +23,28 @@ def create_barplot(inpath, title, filename, errorbar=False):
             filtered_df = df[df['filler_type'] == ftype]
         else:
             filtered_df = df
+        filtered_df.sort_values(by='input', inplace=True)
         if filtered_df['mean'].mean() > 0.0:
-            ax = sns.barplot(data=filtered_df, x='input_condition', y='mean', hue='input',
-                             palette=sns.color_palette('colorblind'))
+            sns.set_context('paper', rc={'axes.titlesize': 16, 'axes.labelsize': 14, 'xtick.labelsize': 10,
+                                         'ytick.labelsize': 12, 'legend.fontsize': 12, 'legend.title_fontsize': 12})
+            fig, ax = plt.subplots(figsize=(10, 7))
+            ax = sns.barplot(data=filtered_df, x='input_condition', y='mean', hue='condition')
             if errorbar:
                 plt.errorbar(x=filtered_df['input_condition'], y=filtered_df['mean'],
                              yerr=filtered_df['std'], fmt='none', c='black', capsize=2)
-            ax.get_legend().set_visible(False)
-            ax.set_title(f'{title} {mapping[ftype]}')
-            # ax.set_ylim(ymax=1.0)
+            ax.set_title(f'{modelname} {mapping[ftype]} {measure} per dataset and condition')
+            conditions = list(filtered_df['input'].unique())
+            items = range(len(filtered_df['input_condition'].unique()))
+            tick_pos = [x - 0.5 for x in list(items[1::2])]
+            ax.set_xticks(tick_pos, labels=conditions)
+            sns.move_legend(ax, 'upper left', bbox_to_anchor=(1, 1))
+            ax.set_ylim(ymax=1.0)
+            ax.yaxis.grid(True)
+            # add vertical lines after every two boxes (1 dataset)
+            for pos in tick_pos:
+                ax.axvline(pos + 1, linewidth=0.5, color='grey')
+            plt.xlabel('Dataset')
+            plt.ylabel(f'Mean {measure}')
             plt.xticks(rotation=45, ha='right', rotation_mode='anchor', fontsize=10)
             plt.tight_layout()
             outpath = inpath.replace('results', 'plots')
@@ -40,20 +52,52 @@ def create_barplot(inpath, title, filename, errorbar=False):
             outpath = outpath[:-cutoff]
             if not os.path.exists(outpath):
                 os.makedirs(outpath)
-            print(f'INPATH: {inpath}')
-            print(f'OUTPATH: {outpath}')
-            plt.savefig(f'{outpath}/{ftype}-{filename}')
+            filename = f'{modelname}-mean_{measure}'
+            plt.savefig(os.path.join(outpath, filename))
             plt.show()
 
 
-def create_boxplot(inpath, title):
-    df = pd.read_csv(inpath, sep='\t')
-    print(df.head())
-    print(df.shape)
-    print(df.columns)
-    df['top_fillers'] = json.loads(df['top_fillers'])
-    df['probabilities'] = json.loads(df['probabilities'])
+def create_boxplot(dir, modelname, measure):
+    # plt.clf()
+    dfs = []
+    for file in os.listdir(dir):
+        print(file)
+        if file != 'statistics.tsv':
+            df = pd.read_csv(os.path.join(dir, file), sep='\t')
+            dfs.append(df)
+            source = file[:-12]
+            df['input'] = [source for x in range(df.shape[0])]
+    merged_df = pd.concat(dfs)
+    merged_df['input_condition'] = merged_df['input'] + '_' + merged_df['condition']
+    merged_df.sort_values(by='input', inplace=True)
+    print(merged_df.head())
+    print(merged_df.shape)
+    print(merged_df.columns)
+    sns.set_context('paper', rc={'axes.titlesize': 16, 'axes.labelsize': 14, 'xtick.labelsize': 10,
+                                 'ytick.labelsize': 12, 'legend.fontsize': 12, 'legend.title_fontsize': 12})
+    fig, ax = plt.subplots(figsize=(10, 7))
+    ax = sns.boxplot(data=merged_df, x='input_condition', y='dom_prob', hue='condition')#, ax=ax)
+    ax.set_title(f'{modelname} DOM {measure} per dataset and condition')
+    conditions = list(merged_df['input'].unique())
+    items = range(len(merged_df['input_condition'].unique()))
+    tick_pos = [x - 0.5 for x in list(items[1::2])]
+    ax.set_xticks(tick_pos, labels=conditions)
+    sns.move_legend(ax, 'upper left', bbox_to_anchor=(1, 1))
+    ax.yaxis.grid(True)
+    # add vertical lines after every two boxes (1 dataset)
+    for pos in tick_pos:
+        ax.axvline(pos+1, linewidth=0.5, color='grey')
+    plt.xlabel('Dataset')
+    plt.ylabel('Probability')
+    plt.xticks(rotation=45, ha='right', rotation_mode='anchor', fontsize=10)
+    plt.tight_layout()
+    outpath = dir.replace('results', 'plots').replace(f'{modelname}/', '')
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+    filename = f'{modelname}-{measure}-boxplot.png'
+    # plt.savefig(os.path.join(outpath, filename))
+    plt.show()
 
 
-filepath = os.path.join(pathlib.Path(__file__).parent.absolute(), '..', 'results/sentence-score/BETO/statistics.tsv')
-create_barplot(filepath, 'BETO mean discrepancy', 'BETO-mean-discrepancy.png', errorbar=False)
+filepath = os.path.join(pathlib.Path(__file__).parent.absolute(), '..', 'results/fill-mask/dom-masking/mBERT/')
+create_boxplot(filepath, modelname='mBERT', measure='probability')
