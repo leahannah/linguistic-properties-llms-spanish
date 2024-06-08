4e00552bac8a6d59262d3be68c9eeab27a95a926
leahannah <gauner.erle@gmail.com>
2024-06-08 15:11:02 +0200

    add all barplots

--- src/analyze.py
+++ src/analyze.py
@@ -6,7 +6,11 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def create_barplot(inpath, modelname, measure, errorbar=False):
+def create_barplot(inpath, modelname, errorbar=False):
+    measure = 'probability' if 'fill-mask' in inpath else 'discrepancy'
+    add_str = ''
+    if 'dobject-masking' in inpath:
+        add_str = ' unmarked' if 'unmarked' in inpath else ' dom'
     df = pd.read_csv(inpath, sep='\t')
     print(df.head())
     print(df.shape)
@@ -22,25 +26,32 @@ def create_barplot(inpath, modelname, measure, errorbar=False):
             filtered_df = df[df['filler_type'] == ftype]
         else:
             filtered_df = df
-        filtered_df.sort_values(by='input_condition', inplace=True)
+        filtered_df.sort_values(by='input', inplace=True)
         if filtered_df['mean'].mean() > 0.0:
-            sns.set_context('paper', rc={'axes.titlesize': 16, 'axes.labelsize': 14, 'xtick.labelsize': 10,
+            sns.set_context('paper', rc={'axes.titlesize': 18, 'axes.labelsize': 14, 'xtick.labelsize': 10,
                                          'ytick.labelsize': 12, 'legend.fontsize': 12, 'legend.title_fontsize': 12})
             fig, ax = plt.subplots(figsize=(10, 7))
-            ax = sns.barplot(data=filtered_df, x='input_condition', y='mean', hue='condition')
+            ax = sns.barplot(data=filtered_df, x='input_condition', y='mean', hue='condition', ax=ax)
             if errorbar:
                 plt.errorbar(x=filtered_df['input_condition'], y=filtered_df['mean'],
                              yerr=filtered_df['std'], fmt='none', c='black', capsize=2)
-            ax.set_title(f'{modelname} {mapping[ftype]} {measure} per dataset and condition')
-            conditions = list(filtered_df['input'].unique())
-            items = range(len(filtered_df['input_condition'].unique()))
-            tick_pos = [x - 0.5 for x in list(items[1::2])]
-            ax.set_xticks(tick_pos, labels=conditions)
+            ax.set_title(f'{modelname} {mapping[ftype]} {measure}{add_str}', pad=20, loc='center')
             sns.move_legend(ax, 'upper left', bbox_to_anchor=(1, 1))
-            ax.set_ylim(ymax=1.0)
+            if measure == 'probability':
+                ax.set_ylim(ymax=1.0)
+            inputs = list(filtered_df['input'])
+            positions = []
+            stepsize = -1.5
+            for inp in list(filtered_df['input'].unique()):
+                stepsize += inputs.count(inp)
+                if stepsize < 0:
+                    stepsize = 0.5
+                positions.append(stepsize)
+            ax.set_xticks(positions, labels=list(filtered_df['input'].unique()))
+            ax.set_axisbelow(True)
             ax.yaxis.grid(True)
             # add vertical lines after every two boxes (1 dataset)
-            for pos in tick_pos:
+            for pos in positions:
                 ax.axvline(pos + 1, linewidth=0.5, color='grey')
             plt.xlabel('Dataset')
             plt.ylabel(f'Mean {measure}')
@@ -51,12 +62,13 @@ def create_barplot(inpath, modelname, measure, errorbar=False):
             outpath = outpath[:-cutoff]
             if not os.path.exists(outpath):
                 os.makedirs(outpath)
-            filename = f'{modelname}-mean_{measure}'
+            filename = f'{modelname}-mean_{ftype}_{measure}-{add_str.strip()}'
             plt.savefig(os.path.join(outpath, filename))
             plt.show()
 
 
-def create_boxplot(dir, modelname, measure):
+def create_boxplot(dir, modelname):
+    measure = 'probability' if 'fill-mask' in dir else 'discrepancy'
     dfs = []
     for file in os.listdir(dir):
         if file != 'statistics.tsv':
@@ -70,22 +82,27 @@ def create_boxplot(dir, modelname, measure):
     print(merged_df.head())
     print(merged_df.shape)
     print(merged_df.columns)
-    sns.set_context('paper', rc={'axes.titlesize': 16, 'axes.labelsize': 14, 'xtick.labelsize': 10,
+    sns.set_context('paper', rc={'axes.titlesize': 18, 'axes.labelsize': 14, 'xtick.labelsize': 10,
                                  'ytick.labelsize': 12, 'legend.fontsize': 12, 'legend.title_fontsize': 12})
     fig, ax = plt.subplots(figsize=(10, 7))
     ax = sns.boxplot(data=merged_df, x='input_condition', y='dom_prob', hue='condition', ax=ax)
-    ax.set_title(f'{modelname} DOM {measure} per dataset and condition')
-    conditions = list(merged_df['input'].unique())
-    items = range(len(merged_df['input_condition'].unique()))
-    tick_pos = [x - 0.5 for x in list(items[1::2])]
-    ax.set_xticks(tick_pos, labels=conditions)
+    ax.set_title(f'{modelname} DOM {measure}')
+    inputs = list(merged_df['input'])
+    positions = []
+    stepsize = -1.5
+    for inp in list(set(inputs)):
+        stepsize += inputs.count(inp)
+        if stepsize < 0:
+            stepsize = 0.5
+        positions.append(stepsize)
+    ax.set_xticks(positions, labels=list(set(inputs)))
     sns.move_legend(ax, 'upper left', bbox_to_anchor=(1, 1))
     ax.yaxis.grid(True)
     # add vertical lines after every two boxes (1 dataset)
-    for pos in tick_pos:
-        ax.axvline(pos+1, linewidth=0.5, color='grey')
+    for pos in positions:
+        ax.axvline(pos + 1, linewidth=0.5, color='grey')
     plt.xlabel('Dataset')
-    plt.ylabel('Probability')
+    plt.ylabel(measure)
     plt.xticks(rotation=45, ha='right', rotation_mode='anchor', fontsize=10)
     plt.tight_layout()
     outpath = dir.replace('results', 'plots').replace(f'{modelname}/', '')
@@ -95,5 +112,31 @@ def create_boxplot(dir, modelname, measure):
     plt.savefig(os.path.join(outpath, filename))
     plt.show()
 
-filepath = os.path.join(pathlib.Path(__file__).parent.absolute(), '..', 'results/fill-mask/dom-masking/mBERT/')
-create_boxplot(filepath, modelname='mBERT', measure='probability')
+
+def create_plots_for_dir(dir_path, type):
+    if type == 'bar':
+        create_plot = create_barplot
+    elif type == 'box':
+        create_plot = create_boxplot
+    else:
+        print(f'Choose a valid plot type, either bar or box')
+        return
+    for subdir, dirs, files in os.walk(dir_path):
+        for file in files:
+            print(subdir)
+            print(file)
+            if file == 'statistics.tsv':
+                if 'BETO' in subdir:
+                    modelname = 'BETO'
+                else:
+                    modelname = 'mBERT'
+                if type == 'bar':
+                    path = os.path.join(subdir, file)
+                else:
+                    path = subdir
+                create_plot(path, modelname=modelname)
+
+
+dir = os.path.join(pathlib.Path(__file__).parent.absolute(), '..',
+                        'results/')
+create_plots_for_dir(dir, type='bar')
